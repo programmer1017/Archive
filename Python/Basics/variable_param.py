4156b2b72d25c6b6e64fcbc6c40c72bee5dc5b29
Junhyuk Lee <junh071017@gmail.com>
2020-12-24 14:40:46 +0900

    ⠀

--- Python/Basics/variable_param.py
+++ Python/Basics/variable_param.py
@@ -1,10 +1,4 @@
-def print_n_times(n, *values):  # 가변 매개변수 함수
-    # do it for n time(s)
-    for i in range(n):
-
-        for value in values:
-            print(value)
-        print()  # just a line switch
 
 
-print_n_times(3, "안녕하세요", "즐거운", "파이썬 프로그래밍")
+for i in range(1, 20):
+    print("*" * i)
