a0c7396edd62b1a9be44240627fe3d728c6ed14f
Junhyuk Lee <junh071017@gmail.com>
2021-01-31 18:15:57 +0900

    ver 0.2

--- Python/Algebra/최소공배수 구하기.py
+++ Python/Algebra/최소공배수 구하기.py
@@ -2,8 +2,15 @@ number_1 = int(input('Enter first number> '))
 number_2 = int(input('Enter second number> '))
 
 
-def amin():
+def main():
     x = 1
 
-    if number_1 * x == number_2 * 1:
-        pass
+    while True:
+        if number_1 * x == number_2 * x:
+            print(number_1 * x)
+            break
+        else:
+            x = x+1
+
+
+main()
