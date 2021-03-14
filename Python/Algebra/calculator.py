394cf83fa24934b21c4060d2786b131e19d41d8f
Junhyuk Lee <junh071017@gmail.com>
2021-03-14 13:50:35 +0900

    Code improvements

--- Python/Algebra/calculator.py
+++ Python/Algebra/calculator.py
@@ -5,28 +5,33 @@ operator = input('Enter operator(+, -, *, /)> ')
 right_int = int(input('Enter right side number of equal sign> '))
 
 
+def before_answer():
+    print("The answer is: ")
+    print()
+
+
 def addition():
+    before_answer()
     print(left_int + right_int)
 
 
 def subtraction():
+    before_answer()
     print(left_int - right_int)
 
 
 def multiplication():
+    before_answer()
     print(left_int * right_int)
 
 
 def division():
+    before_answer()
     print(left_int / right_int)
 
 
 # end of defining functions...
-
-if operator == '=':
-    addition()
-
-elif operator == '-':
+if operator == '-':
     subtraction()
 
 elif operator == '*':
@@ -34,6 +39,9 @@ elif operator == '*':
 
 elif operator == '/':
     division()
+
+elif operator == '+':
+    addition()
 
 else:
     print('Something went wrong... Try again.')
