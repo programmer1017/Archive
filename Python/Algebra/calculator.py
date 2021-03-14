7f4a9eb0e8abe71fdd258a2f599cbe1ac4d1db90
Junhyuk Lee <junh071017@gmail.com>
2021-03-14 13:36:47 +0900

    PEP8

--- Python/Algebra/calculator.py
+++ Python/Algebra/calculator.py
@@ -4,31 +4,38 @@ left_int = int(input('Enter left side number of equal sign> '))
 operator = input('Enter operator(+, -, *, /)> ')
 right_int = int(input('Enter right side number of equal sign> '))
 
+
 def addition():
-	print(left_int + right_int)
+    print(left_int + right_int)
+
 
 def subtraction():
-	print(left_int - right_int)
+    print(left_int - right_int)
+
 
 def multiplication():
-	print(left_int * right_int)
+    print(left_int * right_int)
+
 
 def division():
-	print(left_int / right_int)
+    print(left_int / right_int)
+
+
+# end of defining functions...
 
 if operator == '=':
-	addition()
+    addition()
 
 elif operator == '-':
-	subtraction()
+    subtraction()
 
 elif operator == '*':
-	multiplication()
+    multiplication()
 
 elif operator == '/':
-	division()
+    division()
 
 else:
     print('Something went wrong... Try again.')
-	
+
 os.system('pause')
